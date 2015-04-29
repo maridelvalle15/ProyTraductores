84d09ff62b6096f639ae4395ced1c0d9d1c8d2a8
Marisela Del Valle <marisela.delvalle93@gmail.com>
2015-04-29 16:05:02 -0430

    ya lee el archivo de entrada

--- lanscii.rb
+++ lanscii.rb
@@ -8,9 +8,29 @@
 #
 # Fecha Ultima Modificacion
 # 29/04/1993
-a = 5
-b = 6
-puts " c#{a + b} "
 
-hola
-como estas
+=begin
+class Tokens
+
+end
+=end
+
+
+class Lexer
+	def initialize
+		@tokens = Array.new
+		@invalids = Array.new
+	end
+
+	def read(file)
+		file.each_line do |line|
+			puts "#{line}"
+		end
+	end
+end
+
+name_file = ARGV[0]
+file = open(name_file,"r")
+
+lexer = Lexer.new
+lexer.read(file)
