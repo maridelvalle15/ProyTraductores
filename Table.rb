71ee23593694bd2a780313d57f01965a50756ca0
andres <andres@TNT.ac.labf.usb.ve>
2015-06-08 22:39:30 -0430

    Clase Table y TableSymbol Verificada

--- Table.rb
+++ Table.rb
@@ -7,37 +7,129 @@
 # 	Marisela Del Valle  11-10217
 #
 # Fecha Ultima Modificacion: 
-# 	31/05/2015
+# 	08/06/2015
 
 # Archivo main del proyecto
 
+require "./TableSymbol.rb"
+
 class Table
 	def initialize
-		@symbols = Hash.new()
-		@actual = @symbols
-		@ant = @symbols
+		@actual = TableSymbol.new()
 	end
 
 	def insert(symbol, identifier)
-		if !@symbols.has_key?(identifier)
-			@symbols[identifier] = symbol
-		else
-			puts "Elemento ya encontrado en la table"
+		@actual.insert(symbol, identifier)
+	end
+
+	def delete(identifier)
+		aux = @actual
+		while aux != nil do
+			if aux.contains(identifier)
+				aux.delete(identifier)
+				break
+			end
+			aux = aux.get_father
 		end
 	end
 
-	def get_symbols
-		return @symbols
+	def update(symbol,identifier,value)
+		aux = @actual
+		while aux != nil do
+			if aux.contains(identifier)
+				aux2 = aux.get_symbols
+				array = aux2[identifier]
+				if array[0] == symbol
+					aux.update(symbol,identifier,value)
+					break
+				end
+			end
+			aux = aux.get_father
+		end
 	end
 
-	def print_table
-		puts @symbols
+	def contains(identifier)
+		aux = @actual
+		while aux != nil do
+			if aux.contains(identifier)
+				return true
+			end
+			aux = aux.get_father
+		end
+		return false
+	end
+
+	def lookup(identifier)
+		aux = @actual
+		while aux != nil do
+			if aux.contains(identifier)
+				return aux.lookup(identifier)
+			end
+			aux = aux.get_father
+		end
+		return false
+	end
+
+
+	def addscope
+		new_table = TableSymbol.new()
+		@actual.change_son(new_table)
+		new_table.change_father(@actual)
+		@actual = new_table
+	end
+
+	def endscope
+		@actual = @actual.get_father()
+	end
+
+	def print_actual
+		@actual.print_symbols
+	end
+
+	def print 
+		aux = @actual
+		while aux != nil do
+			puts "Tabla"
+			aux.print_symbols
+			aux = aux.get_father()
+		end
 	end
 end
 
 
-hola = Table.new()
-hola.insert(:INT,3)
-puts "#{hola.get_symbols()}"
-#hola.insert(:INT,hola);
 
+=begin
+tabla = Table.new()
+tabla.insert(:INT,3)
+tabla.insert(:INT,6)
+tabla.print_actual()
+tabla.addscope()
+tabla.insert(:MIa,5)
+tabla.print()
+puts
+tabla.delete(3)
+tabla.print()
+puts
+tabla.endscope()
+tabla.insert(:MEGA,55)
+tabla.print()
+puts
+tabla.addscope()
+tabla.insert(:DACA,554)
+tabla.print()
+puts
+tabla.update(:DACA,554,1)
+tabla.print()
+puts tabla.contains(554)
+puts tabla.contains(6)
+puts tabla.contains("andres")
+print tabla.lookup(554)
+puts
+print tabla.lookup(6)
+puts
+
+#tabla.delete(3)
+#tabla.delete(3)
+#tabla.print_actual()
+#hola.insert(:INT,hola);
+=end
---
+++ TableSymbol.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/env ruby
+#
+# Proyecto Traductores
+# 
+# Integrantes:
+# 	Andres Navarro      11-10688
+# 	Marisela Del Valle  11-10217
+#
+# Fecha Ultima Modificacion: 
+# 	08/06/2015
+
+# Archivo main del proyecto
+
+
+class TableSymbol
+	def initialize
+		@symbols = Hash.new()
+		@father = nil
+		@son = nil
+	end
+
+	def change_father(father)
+		@father = father
+	end
+
+	def change_son(son)
+		@son = son
+	end
+
+	def get_father()
+		return @father 
+	end
+
+	def get_son()
+		return @son 
+	end
+
+
+	def insert(symbol, identifier)
+		if !@symbols.has_key?(identifier)
+			@symbols[identifier] = [symbol,nil]
+		else
+			puts "Elemento ya encontrado en la tabla"
+		end
+	end
+
+	def delete(identifier)
+		if @symbols.has_key?(identifier)
+			@symbols.delete(identifier)
+		else
+			puts "Elemento no se encuentra en la tabla"
+		end
+	end
+
+	def update(symbol,identifier,value)
+		if @symbols.has_key?(identifier)
+			@symbols[identifier] = [symbol,value]
+		else
+			puts "Elemento ya encontrado en la tabla"
+		end
+	end
+
+	def contains(identifier)
+		return  @symbols.has_key?(identifier)
+	end
+
+	def lookup(identifier)
+		if @symbols.has_key?(identifier)
+			return @symbols[identifier]
+		else
+			print "Elemento no se encontra en la tabla"
+		end
+	end
+
+	def get_symbols
+		return @symbols
+	end
+
+	def print_symbols
+		@symbols.each { |key,value| puts "#{key} #{value}" }
+	end
+end
